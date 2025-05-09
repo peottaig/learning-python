53bb35df5c4a143bc39be0a8a4a8a92ebb205075
luiggi peotta <luiggi.peotta@gmail.com>
2025-05-09 09:15:27 -0300

    Aprendizados

---
+++ Learning/Aprendizados/itertools.py
@@ -0,0 +1,57 @@
+# Combinations, Permutations e Product - Itertools
+# Combinação - Ordem não importa - iterável + tamanho do grupo
+# Permutação - Ordem importa
+# Produto - Ordem importa e repete valores únicos
+from itertools import groupby
+from itertools import combinations, permutations, product
+
+
+def print_iter(iterator):
+
+    print(*list(iterator), sep='\n')
+    print()
+
+
+pessoas = [
+    'João', 'Joana', 'Luiz', 'Letícia',
+]
+
+camisetas = [
+    ['preta', 'branca'],
+    ['p', 'm', 'g'],
+    ['masculino', 'feminino', 'unisex'],
+    ['algodão', 'poliéster']
+
+]
+print_iter(combinations(pessoas, 2))
+print_iter(permutations(pessoas, 2))
+print_iter(product(*camisetas))
+
+# groupby - agrupando valores (itertools)
+
+
+alunos = [
+    {'nome': 'Luiz', 'nota': 'A'},
+    {'nome': 'Letícia', 'nota': 'B'},
+    {'nome': 'Fabrício', 'nota': 'A'},
+    {'nome': 'Rosemary', 'nota': 'C'},
+    {'nome': 'Joana', 'nota': 'D'},
+    {'nome': 'João', 'nota': 'A'},
+    {'nome': 'Eduardo', 'nota': 'B'},
+    {'nome': 'André', 'nota': 'A'},
+    {'nome': 'Anderson', 'nota': 'C'},
+]
+
+
+def ordena(aluno):
+    return aluno['nota']
+
+
+alunos_agrupados = sorted(alunos, key=ordena)
+grupos = groupby(alunos_agrupados, key=ordena)
+
+for chave, grupo in grupos:
+    print(chave)
+
+    for aluno in grupo:
+        print(aluno)
